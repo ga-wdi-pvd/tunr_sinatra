89e373d8687a4ee95d460a8bcf3a251eacc15f01
jtellis <jessetellis@gmail.com>
2016-12-14 09:53:58 -0500

    Allow user to create and delete artists

--- app.rb
+++ app.rb
@@ -3,12 +3,38 @@ require 'sinatra/reloader'
 require 'active_record'
 
 # Load the file to connect to the DB
+require_relative 'db/connection'
 
 # Load specific routes / controllers
 
 # Load models
-
+require_relative 'models/artist'
+require_relative 'models/song'
 
 ####################
 #  General routes  #
 ####################
+get '/artists' do
+  @artists = Artist.all
+  erb :'artists/index'
+end
+
+get '/artists/new' do
+  erb :'artists/new'
+end
+
+get '/artists/:id' do
+  @artist = Artist.find(params[:id])
+  erb :'artists/show'
+end
+
+delete '/artists' do
+  @artist = Artist.find(params[:id])
+  @artist.destroy
+  redirect to('/artists')
+end
+
+post '/artists' do
+  @artist = Artist.create(params[:artist])
+  redirect to("/artists/#{@artist[:id]}")
+end
