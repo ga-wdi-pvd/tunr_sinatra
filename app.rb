20a6c3a284ee9ede79f0fb7fd7a47b13debe075e
awanderlyss <alyssa.felix7@gmail.com>
2016-12-14 08:52:01 -0500

    Add new artist page

--- app.rb
+++ app.rb
@@ -3,12 +3,34 @@ require 'sinatra/reloader'
 require 'active_record'
 
 # Load the file to connect to the DB
-
+require_relative 'db/connection'
 # Load specific routes / controllers
 
 # Load models
-
+require_relative 'models/artist'
+require_relative 'models/song'
 
 ####################
 #  General routes  #
 ####################
+
+# get all artists
+get "/artists" do
+  @artists = Artist.all
+  erb :"artists/index"
+end
+
+get "/artists/new" do
+  erb :"artists/new"
+end
+
+#get info for a single artists
+get "/artists/:id" do
+  @artist = Artist.find(params[:id])
+  erb :"artists/show"
+end
+
+post "/artists" do
+  @artist = Artist.create(params[:artist])
+  redirect "/artists/#{@artist.id}"
+end
